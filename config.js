9bb80f65c08d7a638bbe4c14b9f7d4b507f5f41e
Unknown <yassir.chakiri12@gmail.com>
2018-06-06 13:23:31 +0200

    get api

---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by mohamedchakiri on 06/06/2018.
+ */
+
+module.exports = {
+    consumer_key: '7qq7C38i5xqycUJDt4yaQruA6',
+    consumer_secret: 'Y3VDpXWLvCPxKSFX8lfhQ1aPeEH1dz0E7XxT5uWskSjihFdKSr',
+    access_token_key: '4016293643-fH97vhAxTxgOSPDvoXq5Q4snX7NCcHqkcMq9sEr',
+    access_token_secret: 'onIFWLsPdtBrhXZvPNUfYr82sW4CmT5w8uhLLHZHypIfw'
+}
