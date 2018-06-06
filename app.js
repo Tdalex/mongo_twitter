8cd8195125643e9c72f1b6bef53e54ea1b87eae2
Unknown <yassir.chakiri12@gmail.com>
2018-06-06 13:27:58 +0200

    display results

--- app.js
+++ app.js
@@ -21,19 +21,6 @@ T.get('search/tweets', params, function(err, data, response) {
             // Get the tweet Id from the returned data
             var id = { id: data.statuses[i].id_str }
             console.log(data.statuses[i]);
-            // Try to Favorite the selected Tweet
-            /*T.post('favorites/create', id, function(err, response){
-                // If the favorite fails, log the error message
-                if(err){
-                    console.log(err[0].message);
-                }
-                // If the favorite is successful, log the url of the tweet
-                else{
-                    var username = response.user.screen_name;
-                    var tweetId = response.id_str;
-                    console.log('Favorited: ', "https://twitter.com/${username}/status/${tweetId}")
-                }
-            });*/
         }
     } else {
         console.log(err);
