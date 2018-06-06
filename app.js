70020ca1acce8a95f4e03c81551285bd30b50c55
Unknown <yassir.chakiri12@gmail.com>
2018-06-06 14:28:42 +0200

    insert mongodb

--- app.js
+++ app.js
@@ -3,9 +3,13 @@
  */
 
 var Twitter = require('twitter');
+var mongo = require('mongodb');
 var config = require('./config.js');
 
 var T = new Twitter(config);
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
 var params = {
     q: '#worldcup',
@@ -14,15 +18,36 @@ var params = {
     lang: 'fr'
 }
 
-T.get('search/tweets', params, function(err, data, response) {
-    if(!err){
-        // Loop through the returned tweets
-        for(var i = 0; i < data.statuses.length; i++){
-            // Get the tweet Id from the returned data
-            var id = { id: data.statuses[i].id_str }
-            console.log(data.statuses[i]);
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mongo_twitter");
+
+    dbo.createCollection("listTweets", function(err, res) {
+        if (err) throw err;
+        console.log("Collection created!");
+        //db.close();
+    });
+
+    T.get('search/tweets', params, function(err, data, response) {
+        if(!err){
+            // Loop through the returned tweets
+            for(var i = 0; i < data.statuses.length; i++){
+                // Get the tweet Id from the returned data
+                //console.log(data.statuses[i]);
+
+                dbo.collection("listTweets").insertOne(data.statuses[i], function(err, res) {
+                    if (err) throw err;
+                    console.log("1 document inserted");
+
+                });
+             }
+        } else {
+            console.log(err);
         }
-    } else {
-        console.log(err);
-    }
-})
+        db.close();
+    })
+
+
+
+});
