e1c69d39749b3655d618dd4d2e77bfbe4d5ae515
Unknown <yassir.chakiri12@gmail.com>
2018-06-07 11:17:56 +0200

    views

--- app.js
+++ app.js
@@ -5,6 +5,19 @@
 var Twitter = require('twitter');
 var mongo = require('mongodb');
 var config = require('./config.js');
+var express = require('express');
+
+
+var app = express();
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/views'));
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+app.listen(8080);
 
 var T = new Twitter(config);
 
